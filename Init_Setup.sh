7f4fb4b78aa2880b159c04f5ad4ef6beaec4fcaf
Bui An Khang <55547021+Achicken7301@users.noreply.github.com>
2023-11-14 13:26:01 +0700

    Update Init_Setup.sh

--- Init_Setup.sh
+++ Init_Setup.sh
@@ -5,14 +5,14 @@ if [ ! -d "./env" ]; then
 	echo "The 'env' directory does not exist in the current folder."
 	echo "Start create env for python"
 	# python -m venv env	
-	unzip env.zip
+	unzip -q env.zip
 fi
 
 
 # Check if pandoc.exe exist
 #!/bin/sh
 if [ ! -f ./pandoc.exe ]; then
-    unzip pandoc.zip
+    unzip -q pandoc.zip
 fi
 
 
@@ -20,7 +20,7 @@ fi
 echo "Init use env"
 # NOTE: Check if this is window or linux
 # Actiavte python env
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 . ./env/Scripts/activate
 
 echo "Install from requirements"
