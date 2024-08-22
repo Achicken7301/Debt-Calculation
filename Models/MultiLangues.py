872b3abef35d3403729ea2e27686a19044e501e4
Bui An Khang <banhbeovodung01@gmail.com>
2024-08-22 20:49:38 +0700

    [fix] auto create missing file/folder
    example.conf
    translate folder

--- Models/MultiLangues.py
+++ Models/MultiLangues.py
@@ -2,18 +2,34 @@ import json
 from Models.Global import ErrorHandler
 from Models.ProgramConfig import ProgramConfig
 import os
+import requests
 
 
 class MultiLanguages:
     def __init__(self) -> None:
         self.language = ProgramConfig().read("DEFAULT", "language")
+        # If no translate folder -> create one
+        trans_folder = "translate"
+        if not os.path.exists(trans_folder):
+            os.makedirs(trans_folder)
 
-        self.load()
+        # Check languaage .json
+        if not os.path.isfile(f"{trans_folder}/{self.language}.json"):
+            # Download from git repo
+            language_json_url = f"https://raw.githubusercontent.com/Achicken7301/Debt-Calculation/develop/translate/{self.language}.json"
+            language_json_reponse = requests.get(language_json_url)
+            if language_json_reponse.status_code == 200:
+                with open(
+                    f"{os.getcwd()}/{trans_folder}/{self.language}.json", "wb"
+                ) as file:
+                    file.write(language_json_reponse.content)
+
+        self.load(self.language)
 
     def get_locale(self) -> str:
         return self.language
 
-    def load(self) -> ErrorHandler:
+    def load(self, my_lang) -> ErrorHandler:
         """
         Load data translate from folder translate with name is `self.language`
 
@@ -21,7 +37,7 @@ class MultiLanguages:
             ErrorHandler: _description_
         """
         # TODO: Find if there self.language file in translate folder, if not return FILE_NOT_FOUND
-        language_file = f"translate/{self.language}.json"
+        language_file = f"translate/{my_lang}.json"
         with open(language_file, "r", encoding="utf8") as jsonfile:
             self.trans_data = json.load(jsonfile)
 
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+from urllib import response
 from PyQt5 import QtGui, QtCore, QtWidgets
+import requests
 import sys
 from views.MainWindow import MainWindow
 import tabulate
 import openpyxl
+import os
+
+example_conf_url = "https://raw.githubusercontent.com/Achicken7301/Debt-Calculation/develop/example.conf"
+translate_url = (
+    "https://github.com/Achicken7301/Debt-Calculation/tree/develop/translate"
+)
 
 if __name__ == "__main__":
-    # Check file (.conf,...) if not create one.
+    # Download initial files
+    if not os.path.isfile("example.conf"):
+        example_conf_reponse = requests.get(example_conf_url)
+        if example_conf_reponse.status_code == 200:
+            with open("example.conf", "wb") as file:
+                file.write(example_conf_reponse.content)
+
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.setWindowTitle("Debt Calculation")
